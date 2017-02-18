35d91fa4ac1be419c64e32febc92d9b6e5476165
Doug Fleming <doug.g.fleming@gmail.com>
2017-02-18 17:06:28 +1000

    Initial commit

---
+++ lib/vdf_view_tool.rb
@@ -0,0 +1,6 @@
+require "vdf_view_tool/version"
+require "vdf_view_tool/renderer"
+
+module VdfViewTool
+  # Your code goes here...
+end
---
+++ lib/vdf_view_tool/version.rb
@@ -0,0 +1,3 @@
+module VdfViewTool
+  VERSION = "0.1.0"
+end
